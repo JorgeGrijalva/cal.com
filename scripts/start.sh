5f5f8de04a9e2a8028e2d46ee807bd8d77fa7b18
David Sanchez <davidsnjaz@gmail.com>
2025-03-04 16:10:25 -0600

    fix startsh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,5 +7,5 @@ scripts/replace-placeholder.sh "$BUILT_NEXT_PUBLIC_WEBAPP_URL" "$NEXT_PUBLIC_WEB
 
 scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 npx prisma migrate deploy --schema /app/packages/prisma/schema.prisma
-npx ts-node --transpile-only /packages/prisma/seed-app-store.ts
+npx ts-node --transpile-only /app/packages/prisma/seed-app-store.ts
 yarn start
